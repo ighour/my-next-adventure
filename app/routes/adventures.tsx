4b62b2a23a0e4502e2a079d04497052d3df263a3
Celio Rodrigues <celio.rodrigues@instabox.se>
2023-02-21 16:19:27 +0100

    Set navbar

--- app/routes/adventures.tsx
+++ app/routes/adventures.tsx
@@ -1,9 +1,10 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
 
 import { requireUserId } from "~/session.server";
 import { getAdventureListItems } from "~/models/adventure.server";
+import Navbar from "~/components/Navbar";
 
 export async function loader({ request }: LoaderArgs) {
   const userId = await requireUserId(request);
@@ -16,19 +17,7 @@ export default function AdventuresPage() {
 
   return (
     <div className="flex h-full min-h-screen flex-col">
-      <header className="flex items-center justify-between bg-slate-900 p-4 text-white">
-        <h1 className="text-3xl font-bold">
-          <Link to=".">My Adventures</Link>
-        </h1>
-        <Form action="/logout" method="post">
-          <button
-            type="submit"
-            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-          >
-            Logout
-          </button>
-        </Form>
-      </header>
+      <Navbar />
 
       <main className="flex h-full bg-white dark:bg-slate-800 text-black dark:text-white">
         <div className="h-full w-80 border-r dark:border-gray-800 bg-gray-50 dark:bg-gray-900">
