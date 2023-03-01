22b0c7caaff27376116a680e72dfbcb1f57b849f
Celio Rodrigues <celio.rodrigues@instabox.se>
2023-03-01 14:05:49 +0100

    Move challenge component to page

--- app/routes/adventures2/$adventureId/challenges/index.tsx
+++ app/routes/adventures2/$adventureId/challenges/index.tsx
@@ -1,8 +1,10 @@
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useActionData, useLoaderData } from "@remix-run/react";
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import clsx from "clsx";
+import type { ChangeEvent} from "react";
+import { useEffect, useRef, useState } from "react";
 import invariant from "tiny-invariant";
-import ChallengeListItem from "~/components/ChallengeListItem";
 
 import { getAdventure } from "~/models/adventure.server";
 import { completeChallenge, getChallengeListItems, revealChallenge, updateNote } from "~/models/challenge.server";
@@ -59,6 +61,117 @@ export async function action({ request }: ActionArgs) {
 // @TODO - get automatically from action
 export type TActionErrorData = { _challengeId: string, note?: string };
 
+interface IChallengeListItemProps {
+    id: string
+    title: string
+    description: string
+    notePlaceholder: string | null
+    completed: boolean
+    revealed: boolean
+    note: string | null
+    action: {
+        text: string
+        name: string
+    } | null
+    errors?: TActionErrorData
+    className?: string
+};
+
+function ChallengeListItem({ id, title, description, notePlaceholder, completed, revealed, note, action, errors, className }: IChallengeListItemProps) {
+    const [modifyingNote, setModifyingNote] = useState(note ?? "");
+
+    const noteRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        if (errors?.note) {
+            noteRef.current?.focus();
+        }
+    }, [errors]);
+
+    const handleChangeModifyingNote = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        setModifyingNote(() => event.target.value);
+    }
+
+    return (
+        <div className={clsx(className)}>
+            <div className="card lg:card-side bg-base-100 shadow-xl">
+                <figure>
+                    <img
+                        src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+                        alt="TODO"
+                    />
+                </figure>
+                <div className="card-body w-96 h-96">
+                    <h2 className="card-title px-2">{title}</h2>
+                    <div className={clsx("space-y-4 h-64 overflow-y-auto px-1", `${!revealed ? "blur-sm" : ""}`)} dangerouslySetInnerHTML={{ __html: description }} />
+                    {action &&
+                        <div className="card-actions justify-end">
+                            <Form method="post">
+                                <input type="hidden" name="_challengeId" value={id} />
+                                <button
+                                    type="submit"
+                                    name="_action"
+                                    value={action.name}
+                                    className="btn btn-primary"
+                                >
+                                    {action.text}
+                                </button>
+                            </Form>
+                        </div>
+                    }
+                </div>
+            </div>
+            {completed &&
+                <div className="form-control my-2">
+                    <Form method="post">
+                        <input type="hidden" name="_challengeId" value={id} />
+                        <label className="label">
+                            <span className="label-text-alt">{notePlaceholder ?? "How was the challenge?"}</span>
+                        </label>
+                        <textarea
+                            ref={noteRef}
+                            name="note"
+                            className="textarea resize-none w-full h-20 textarea-ghost hover:textarea-secondary"
+                            aria-invalid={errors?.note ? true : undefined}
+                            aria-errormessage={
+                                errors?.note ? `note-error-${id}` : undefined
+                            }
+                            placeholder="Add your notes..."
+                            value={modifyingNote}
+                            onChange={handleChangeModifyingNote}
+                        ></textarea>
+                        {errors?.note && (
+                            <div className="pt-1 text-red-700" id={`note-error-${id}`}>
+                                {errors.note}
+                            </div>
+                        )}
+                        {note && note.length > 0 && modifyingNote.length === 0 &&
+                            <button
+                                type="submit"
+                                name="_action"
+                                value="remove-note"
+                                className="btn btn-xs btn-secondary"
+                            >
+                                Remove note
+                            </button>
+                        }
+                        {note !== modifyingNote && modifyingNote.length !== 0 &&
+                            <button
+                                type="submit"
+                                name="_action"
+                                value="update-note"
+                                className="btn btn-xs btn-secondary"
+                            >
+                                Save note
+                            </button>
+                        }
+                    </Form>
+                </div>
+            }
+        </div>
+    );
+}
+
 export default function ChallengesListPage() {
     const data = useLoaderData<typeof loader>();
     const actionData = useActionData<typeof action>();
@@ -85,9 +198,8 @@ export default function ChallengesListPage() {
 
     return (
         <div className="flex flex-col justify-center items-center">
-            {data.challenges.map((challenge, index) =>
+            {data.challenges.map(challenge =>
                 <ChallengeListItem
-                    index={index}
                     className="mx-2 my-4"
                     key={challenge.id}
                     id={challenge.id}
