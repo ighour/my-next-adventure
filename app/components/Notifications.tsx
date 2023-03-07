2a2fe54720cf72a98ee443e4a431eb1fb399d3e7
Celio Rodrigues <celio.rodrigues@instabox.se>
2023-03-07 22:36:25 +0100

    Fix notification

--- app/components/Notifications.tsx
+++ app/components/Notifications.tsx
@@ -3,7 +3,7 @@ import toast, { Toaster } from 'react-hot-toast';
 export default function Notifications() {
     return (
         <Toaster
-            position="bottom-right"
+            position="bottom-left"
             reverseOrder={false}
             gutter={8}
             containerClassName="max-w-xl"
@@ -26,15 +26,11 @@ export function createNotificationWithTitleAndDescription({ title, description }
     toast.custom(
         <div className="alert shadow-lg">
             <div>
-                {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> */}
                 <div>
                     <h3 className="font-bold">{title}</h3>
                     <div className="text-xs">{description}</div>
                 </div>
             </div>
-            {/* <div className="flex-none">
-                <button className="btn btn-sm">See</button>
-            </div> */}
         </div>
     );
 }
